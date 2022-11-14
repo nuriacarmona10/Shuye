2ace1e370edfeb264e7a7f7e7bb8f69fcfed52d3
Nuriacarmona10 <nuriacarmona10@hotmail.com>
2022-11-14 18:19:13 +0100

    colision with terrain dont work

---
+++ Assets/MeditationManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class MeditationManager : MonoBehaviour
+{
+     XRNodeState m_XRNodeState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  Debug.Log(m_XRNodeState.TryGetPosition(m_XRNodeState.position));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
